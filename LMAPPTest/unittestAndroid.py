77e32d69d0f1fce76b2f1b8de5d19d4d1e28f177
NJNugget <jiangziyunhao@sina.com>
2015-09-10 17:34:24 +0800

    更新android回复帖子功能

--- LMAPPTest/unittestAndroid.py
+++ LMAPPTest/unittestAndroid.py
@@ -74,155 +74,155 @@ class SimpleAndroidTests(unittest.TestCase):
     10、删除订单功能
     11、回复帖子
     '''
-#     def test_addYHP(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入分类页面
-#         self.findElementByText("我的")
-#         self.findElementByText("我的优惠")
-#         self.findElementByText("添加优惠券")
-#         #输入优惠券信息
-#         self.inputString("YHP0009300002785", "123962")
-#         self.findElementByText("加入我的优惠券")
-#     def test_logIn(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入分类页面
-#         self.findElementByText("我的")
-#         els = self.driver.find_elements_by_xpath("//*[@clickable='true']")
-#         before = self.driver.current_activity
-# #         print(before)
-#         els[1].click()
-#         sleep(1)
-#         after = self.driver.current_activity
-# #         print(after)
-#         if(before == after):
-#             els[2].click()
-#             self.findElementByText("确定")
-#             els[1].click()
-#             #     wd.find_element_by_xpath("//*[@clickable='true']").click()
-#         self.inputString("1234", "1234")
-#         self.driver.find_elements_by_xpath("//*[@text='登录']")[1].click()
-#         sleep(2)
-#     def test_buyAtOnce(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入分类页面
-#         self.findElementByText("分类")
-#         self.findElementByText("酒店")
-#         self.driver.find_element_by_xpath("//android.widget.ListView/android.widget.LinearLayout/android.widget.FrameLayout").click()
-#         self.findElementByText("立即预订")
-#         sleep(1)
-#         self.scroll_screen(500, 300, 500, 200)
-#         self.findElementByText("选择日期")
-#           
-#         flag = True
-#         while(flag == True):
-#             self.scroll_screen(500,1750,500,1)
-#             for i in range(1,7):
-#                 if(flag==True):
-#                     for j in range(1,8):
-#                         el = self.driver.find_element_by_xpath("//android.widget.LinearLayout[%s]/android.view.View[%s]"%(i,j))
-#                         if(self.activityIsChanged(el)):
-#                             flag = False
-#                             break
-#               
-#         self.findElementByText("提交订单")          
-#     def test_check_order(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入我的页面
-#         self.findElementByText("我的")
-#         self.findElementByText("我的订单")
-#         #查看待付款和申请退款中
-#         self.scroll_screen(900, 1000, 100, 1000)
-#         sleep(1)
-#         self.scroll_screen(900, 1000, 100, 1000)     
-#     def test_check_delete_collection(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入我的收藏页面
-#         self.findElementByText("我的")
-#         self.findElementByText("我的收藏")
-#         #长按第一个收藏，并点击删除
-#         action1 = TouchAction(self.driver)  
-# #         el = self.driver.find_element_by_xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.ListView[1]/android.widget.FrameLayout[1]")
-#         el = self.driver.find_element_by_id("com.qyer.android.lastminute:id/deal_layout")
-#         action1.long_press(el).wait(1500).perform()
-#         self.findElementByText("确定")
-#     def test_check_notice(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入我的提醒页面
-#         self.findElementByText("我的")
-#         self.findElementByText("我的提醒")
-#         els = self.driver.find_elements_by_xpath("//android.widget.ImageView")
-#         els[1].click()
-#         sleep(1)
-#         self.findElementByText("折扣类型")
-#         self.findElementByText("机票")
-#         self.findElementByText("确定")
-#         sleep(3)
-#         self.driver.find_element_by_id("com.qyer.android.lastminute:id/iv_notifi_delete").click()
-#         sleep(2)
-#         self.findElementByText("确定")
-#            
-#     def test_check_Qyer_choiceness(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入我的提醒页面
-#         self.findElementByText("穷游精选")
-#         sleep(5)
-#         self.driver.find_element_by_xpath("//android.widget.ListView[1]/android.widget.FrameLayout[1]").click()
-#         sleep(2)
-#         self.driver.find_element_by_id("com.qyer.android.lastminute:id/llLeftPanle").click()
-#         sleep(3)
-#            
-#     def test_check_search(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         self.driver.find_element_by_id("com.qyer.android.lastminute:id/ic_left_image").click()
-#         self.findElementByText("日本")
-#         sleep(3)
-#            
-#     def test_check_sort(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入分类页面
-#         self.findElementByText("分类")
-#         self.findElementByText("酒店")
-#         self.driver.find_element_by_id("com.qyer.android.lastminute:id/ivOrderType").click()
-#         self.findElementByText("价格从低到高")
-#         sleep(3)
-#     def test_check_delete_order(self):
-#         sleep(8)
-#         #滑动屏幕用以显示更多
-#         self.scroll_screen(500, 500, 700, 70)
-#         #进入我的页面
-#         self.findElementByText("我的")
-#         self.findElementByText("我的订单")
-#         sleep(1)
-#         flag = True
-#         while(flag == True):
-#             try:
-#                 self.findElementByText("订单关闭")
-#                 sleep(3)
-#                 self.findElementByText("删除订单")
-#                 sleep(1)
-#                 self.findElementByText("确定")
-#                 sleep(2)
-#                 flag = False
-#             except:
-#                 self.scroll_screen(500, 1500, 500, 600)
+    def test_addYHP(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入分类页面
+        self.findElementByText("我的")
+        self.findElementByText("我的优惠")
+        self.findElementByText("添加优惠券")
+        #输入优惠券信息
+        self.inputString("YHP0009300002785", "123962")
+        self.findElementByText("加入我的优惠券")
+    def test_logIn(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入分类页面
+        self.findElementByText("我的")
+        els = self.driver.find_elements_by_xpath("//*[@clickable='true']")
+        before = self.driver.current_activity
+#         print(before)
+        els[1].click()
+        sleep(1)
+        after = self.driver.current_activity
+#         print(after)
+        if(before == after):
+            els[2].click()
+            self.findElementByText("确定")
+            els[1].click()
+            #     wd.find_element_by_xpath("//*[@clickable='true']").click()
+        self.inputString("1234", "1234")
+        self.driver.find_elements_by_xpath("//*[@text='登录']")[1].click()
+        sleep(2)
+    def test_buyAtOnce(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入分类页面
+        self.findElementByText("分类")
+        self.findElementByText("酒店")
+        self.driver.find_element_by_xpath("//android.widget.ListView/android.widget.LinearLayout/android.widget.FrameLayout").click()
+        self.findElementByText("立即预订")
+        sleep(1)
+        self.scroll_screen(500, 300, 500, 200)
+        self.findElementByText("选择日期")
+           
+        flag = True
+        while(flag == True):
+            self.scroll_screen(500,1750,500,1)
+            for i in range(1,7):
+                if(flag==True):
+                    for j in range(1,8):
+                        el = self.driver.find_element_by_xpath("//android.widget.LinearLayout[%s]/android.view.View[%s]"%(i,j))
+                        if(self.activityIsChanged(el)):
+                            flag = False
+                            break
+               
+        self.findElementByText("提交订单")          
+    def test_check_order(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入我的页面
+        self.findElementByText("我的")
+        self.findElementByText("我的订单")
+        #查看待付款和申请退款中
+        self.scroll_screen(900, 1000, 100, 1000)
+        sleep(1)
+        self.scroll_screen(900, 1000, 100, 1000)     
+    def test_check_delete_collection(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入我的收藏页面
+        self.findElementByText("我的")
+        self.findElementByText("我的收藏")
+        #长按第一个收藏，并点击删除
+        action1 = TouchAction(self.driver)  
+#         el = self.driver.find_element_by_xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.ListView[1]/android.widget.FrameLayout[1]")
+        el = self.driver.find_element_by_id("com.qyer.android.lastminute:id/deal_layout")
+        action1.long_press(el).wait(1500).perform()
+        self.findElementByText("确定")
+    def test_check_notice(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入我的提醒页面
+        self.findElementByText("我的")
+        self.findElementByText("我的提醒")
+        els = self.driver.find_elements_by_xpath("//android.widget.ImageView")
+        els[1].click()
+        sleep(1)
+        self.findElementByText("折扣类型")
+        self.findElementByText("机票")
+        self.findElementByText("确定")
+        sleep(3)
+        self.driver.find_element_by_id("com.qyer.android.lastminute:id/iv_notifi_delete").click()
+        sleep(2)
+        self.findElementByText("确定")
+            
+    def test_check_Qyer_choiceness(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入我的提醒页面
+        self.findElementByText("穷游精选")
+        sleep(5)
+        self.driver.find_element_by_xpath("//android.widget.ListView[1]/android.widget.FrameLayout[1]").click()
+        sleep(2)
+        self.driver.find_element_by_id("com.qyer.android.lastminute:id/llLeftPanle").click()
+        sleep(3)
+            
+    def test_check_search(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        self.driver.find_element_by_id("com.qyer.android.lastminute:id/ic_left_image").click()
+        self.findElementByText("日本")
+        sleep(3)
+            
+    def test_check_sort(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入分类页面
+        self.findElementByText("分类")
+        self.findElementByText("酒店")
+        self.driver.find_element_by_id("com.qyer.android.lastminute:id/ivOrderType").click()
+        self.findElementByText("价格从低到高")
+        sleep(3)
+    def test_check_delete_order(self):
+        sleep(8)
+        #滑动屏幕用以显示更多
+        self.scroll_screen(500, 500, 700, 70)
+        #进入我的页面
+        self.findElementByText("我的")
+        self.findElementByText("我的订单")
+        sleep(1)
+        flag = True
+        while(flag == True):
+            try:
+                self.findElementByText("订单关闭")
+                sleep(3)
+                self.findElementByText("删除订单")
+                sleep(1)
+                self.findElementByText("确定")
+                sleep(2)
+                flag = False
+            except:
+                self.scroll_screen(500, 1500, 500, 600)
     def test_reply(self):
         sleep(8)  
         self.driver.find_element_by_id("com.qyer.android.lastminute:id/ivSale1").click()
